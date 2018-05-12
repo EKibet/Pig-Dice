75190dfa746f04a9cc62d772f521650ef97c7ad0
Edgar Kibet <kibetedgar@gmail.com>
2018-05-12 19:20:12 +0300

    Adds js code which facilitates the die roll and increasing the current score

--- js/scripts.js
+++ js/scripts.js
@@ -1,116 +1,87 @@
-function Names(name1, name2) {
-  this.name1 = name1;
-  this.name2 = name2;
+//Business Logic
+var firstPlayer;
+var secondPlayer;
+
+var dieRoll=function(){
+  return Math.floor(Math.random()*6)+1;
 }
-
-var pigGame = {
-  player1Score: 0,
-  player2Score: 0,
-  playerUp: 1,
-  turnScore: 0,
-};
-
-function dieRoll () {
-  die1 = Math.floor(Math.random()*6) +1;
-  return die1;
+function ActivePlayer(round){
+  this.roll=0;
+  this.currentScore=0;
+  this.totalScore=0;
+  this.round=round;
+  this.playerName;
 }
-
-var playerRoll = function() {
-  var roll = dieRoll();
-  if(roll ===1){
-    pigGame.turnScore = 0;
-    alertEndTurn();
-    switchPlayer();
-  } else {
-    pigGame.turnScore +=roll;
-    if (pigGame.playerUp === 1) {
-      if (pigGame.turnScore + pigGame.player1Score >= 21) {
-        alertWinner(1);
-      }
-    } else if (pigGame.turnScore + pigGame.player2Score >= 21) {
-      alertWinner(2);
+ActivePlayer.prototype.roll1=function(){
+  if (this.roll===1) {
+    this.currentScore=0;
+    alert(this.playerName+",your roll is a 1! Your turn is over!!")
   }
-  }
-  return roll;
-}
-
-function holdThePig() {
-  var currentPlayer = pigGame.playerUp;
-  if (currentPlayer ===1) {
-    pigGame.player1Score += pigGame.turnScore;
-  } else {
-    pigGame.player2Score += pigGame.turnScore;
-  }
-  pigGame.turnScore = 0;
-  switchPlayer();
-}
-
-
-function switchPlayer () {
-  if (pigGame.playerUp === 1) {
-    $("#player1Button").hide();
-    $("#player2Button").show();
-    pigGame.playerUp = 2;
-
-  } else {
-    $("#player2Button").hide();
-    $("#player1Button").show();
-    pigGame.playerUp = 1;
-
+  else {
+    this.currentScore+=this.roll;
   }
 }
-
-function resetGame() {
-  pigGame.player1Score = 0;
-  pigGame.player2Score = 0;
-  pigGame.playerUp = 1;
-  pigGame.turnScore = 0;
+ActivePlayer.prototype.holdGame=function(){
+  this.totalScore+=this.currentScore;
+  this.currentScore=0;
+  elert(this.playerName+",Change player");
+}
+ActivePlayer.prototype.findWinner=function(){
+  if(this.totalScore>=100){
+    alert("Bravo! "+this.playerName+",you are the winner!");
+  }
+}
+ActivePlayer.prototype.newGame=function(){
+  this.roll=0;
+  this.currentScore=0;
+  this.totalScore=0;
+  this.playerName="";
+}
+var resetField=function(){
+  $(".player1").val("");
+  $(".player2").val("");
 }
 
-// -----BUSINESS LOGIC --- (above) -----
-// ------------------------------------
-// ----USER LOGIC ---- (below)--------
 
+//UI interface Logic
 
-function alertEndTurn(){
-  alert("Sorry - you rolled a 1.  Your score remains the same and your turn is over.");
-  $(".playerStatus").text(pigGame.playerUp);
-}
+$(document).ready(function(){
+  $("button#play").click(function(event){
+    event.preventDefault();
+    firstPlayer= new ActivePlayer(true);
+    secondPlayer= new ActivePlayer(false);
+    var player1Name=$(".player1").val();
+    $("#player1Name").text(player1Name);
+    var player2Name=$(".player2").val();
+    $("#player2Name").text(player2Name);
 
-function alertWinner(playerNumber) {
-  alert("Player " + playerNumber + " is the BIG winner!!");
-  resetGame();
-  $(".gameStatusDisplay").text(0);
-}
-
-$(document).ready(function() {
-
-  $("form#pigForm").submit(function(event){
-    var playerName1 = $("input#playerName1").val();
-    var playerName2 = $("input#playerName2").val();
-      $("span#playerName1").text(playerName1);
-      $("span#playerName2").text(playerName2);
-      $("#player2Button").hide();
-      $("#player1Button").show();
-      $(".playerStatus").text(pigGame.playerUp);
-      event.preventDefault();
-
-    var nameHolder = new Names(playerName1, playerName2);
-  })
-
-
-  $(".rollPig").click(function() {
-    pigResult = playerRoll();
-    $(".rollResult").text(pigResult);
-    $(".turnScore").text(pigGame.turnScore);
-
+    firstPlayer.playerName=player1Name;
+    secondPlayer.playerName=player2Name;
   });
-
-  $(".holdPig").click(function(){
-    holdThePig();
-    $("rollResult").text("");
-    $(".player1Score").text(pigGame.player1Score);
-    $(".player2Score").text(pigGame.player2Score);
-    $(".playerStatus").text(pigGame.playerUp);
+  $("button#playerOneRoll").click(function(event){
+    firstPlayer.roll=dieRoll();
+    $("#die-roll-1").text(firstPlayer.roll);
+    firstPlayer.roll1();
+    $("#round-total-1").text(firstPlayer.currentScore);
+  });
+  $("button#playerTwoRoll").click(function(event){
+    secondPlayer.roll=dieRoll();
+    $("#die-roll-2").text(secondPlayer.roll);
+    secondPlayer.roll1();
+    $("#round-total-2").text(secondPlayer.currentScore);
+  });
+  $("button#playerOneHold").click(function(event){
+    firstPlayer.holdGame();
+    $("#total-score-1").text(firstPlayer.totalScore);
+    $("#round-total-1").empty();
+    $("#die-roll-1").empty();
+    firstPlayer.findWinner();
+  });
+  $("#playerTwoHold").click(function(event){
+    secondPlayer.holdGame();
+    $("#total-score-1").text(secondPlayer.totalScore);
+    $("#total-score-1").text(secondPlayer.totalScore);
+    $("#die-roll-1").empty();
+    secondPlayer.findWinner();
   });
 });
